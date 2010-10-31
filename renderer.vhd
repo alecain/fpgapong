71400b014aabe5de397c851d18d03c35d38e0de6
Andrew LeCain <electricalpanel@gmail.com>
2010-10-31 18:09:05 -0400

    Modified renderer to be combinational rather than a state machine.

--- renderer.vhd
+++ renderer.vhd
@@ -12,7 +12,8 @@ ENTITY renderer IS
 			depth: integer := 1
 			  );
 PORT (
-			clk_in 			: IN std_logic;
+			x 					: IN integer range 0 to 640;
+			y 					: IN integer range 0 to 480;
 			frame_sync		: in std_logic;
 			enable			: in std_logic;
 			
@@ -25,72 +26,47 @@ PORT (
 			ballx				:in integer;
 			bally				:in integer
 			
+			
 		);
 END renderer;
 
 
 architecture behavioral of renderer is
-		signal x : integer range 0 to 640 :=0;
-		signal y : integer range 0 to 480 :=0;
+		--signal x : integer range 0 to 640 :=0;
+		--signal y : integer range 0 to 480 :=0;
 		
 begin			
 
-
-
-
-	process(clk_in)
-	begin
-		if rising_edge(clk_in) then
-			if (frame_sync = '1') then
-				x<=0;
-				y<=0;
-			end if;
-			
-			
-			if (enable = '1') then
-				if (x >= 628) then
-					x<=0;
-					y<=y+1;
-				else 	
-					x<=x+1;
-				end if;
+process(x,y)
+begin
+		if (x<=ballx+10 and
+				x>=ballx-10 and
+				y <=bally+10 and
+				y >= bally-10) then
+				pixel_R<="1111111111";
+				pixel_G<="1111111111";
+				pixel_B<="1111111111";
 				
-				if (y >= 475) then
-					y<=0;
-				end if;
-
-				
-				if (x<=ballx+10 and
-					 x>=ballx-10 and
-					y <=bally+10 and
-					y >= bally-10) then
-					pixel_R<="1111111111";
-					pixel_G<="1111111111";
-					pixel_B<="1111111111";
-					
-				elsif (x<=20+10 and
-					 x>=20-10 and
-					y <=lpaddle+40 and
-					y >= lpaddle-40) then
-					pixel_R<="1111111111";
-					pixel_G<="0000000000";
-					pixel_B<="0000000000";
-				elsif (x<=600+10 and
-					 x>=600-10 and
-					y <=rpaddle+40 and
-					y >=rpaddle-40) then
-					pixel_R<="0000000000";
-					pixel_G<="0000000000";
-					pixel_B<="1111111111";
-			else
-				pixel_R<="0000000000";
-				pixel_G<="0001111111";
+			elsif (x<=20+10 and
+				 x>=20-10 and
+				y <=lpaddle+40 and
+				y >= lpaddle-40) then
+				pixel_R<="1111111111";
+				pixel_G<="0000000000";
 				pixel_B<="0000000000";
-			end if;
+			elsif (x<=600+10 and
+				 x>=600-10 and
+				y <=rpaddle+40 and
+				y >=rpaddle-40) then
+				pixel_R<="0000000000";
+				pixel_G<="0000000000";
+				pixel_B<="1111111111";
+		else
+			pixel_R<="0000000000";
+			pixel_G<="0001111111";
+			pixel_B<="0000000000";
 		end if;
-	
-		end if;
-	
+		
 	end process;
 	--pixel_G<="1111111111";
 
