769fa0c6855b1a78b09ed9a19a0ef087580d2126
Andrew LeCain <electricalpanel@gmail.com>
2010-11-10 13:30:18 -0500

    Added bist

---
+++ BIST.vhd
@@ -0,0 +1,105 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+
+ENTITY BIST IS
+	generic (
+		INPUT_SIZE : 			integer:= 7;
+		OUTPUT_SIZE : 			integer:= 7;
+		REGISTER_SIZE :		integer := 7
+	);
+	
+	PORT (
+		INPUT_IN		: 	IN std_logic_vector (INPUT_SIZE downto 0);
+		INPUT_OUT	: 	OUT std_logic_vector (INPUT_SIZE downto 0);
+		
+		OUTPUT_IN	: 	IN std_logic_vector (OUTPUT_SIZE downto 0);
+		OUTPUT_OUT	: 	OUT std_logic_vector (OUTPUT_SIZE downto 0);
+		
+		VECTOR:			std_logic_vector(REGISTER_SIZE downto 0);
+		
+		CLK			:  IN std_logic;
+		TEST_EN		: 	IN	STD_logic;
+		
+		TEST_DONE	:  OUT std_logic:='0';
+		TEST_FAIL	:  OUT std_logic:='1';
+		VECTOR_OUT	:	OUT std_logic_vector(REGISTER_SIZE downto 0)
+	);
+end BIST;
+
+architecture behavioral of BIST is
+
+	
+	component PRBS_REG is 
+		generic (
+			OUTPUT_SIZE : 			integer:= 8
+		);
+		PORT (
+			INPUT_OUT	: 	OUT std_logic_vector (INPUT_SIZE downto 0);
+			CLK			:  IN std_logic;
+			TEST_EN		: 	IN	STD_logic
+		);
+	end component;
+	
+	
+	signal TEST_RUNNING: std_logic :='0';
+	signal PRBS 		: std_logic_vector (INPUT_SIZE downto 0);
+	signal Comp_in		: std_logic_vector (OUTPUT_SIZE downto 0);
+	signal comp_out	: std_logic_vector (REGISTER_SIZE downto 0);
+	signal counter		: integer :=0 ;
+	signal s_OUTPUT_OUT: std_logic_vector (OUTPUT_SIZE downto 0);
+	
+
+BEGIN
+	generator: PRBS_REG 	generic map (OUTPUT_SIZE => INPUT_SIZE) 
+								port map (PRBS, CLK, TEST_RUNNING);
+								
+	compressor: entity WORK.COMP_REG generic map (INPUT_SIZE=>OUTPUT_SIZE,REGISTER_SIZE=>REGISTER_SIZE)
+								port map (Comp_in,comp_out,CLK, TEST_RUNNING);
+				
+	OUTPUT_OUT <= s_OUTPUT_OUT;
+	--VECTOR_OUT <= comp_out;
+	
+	
+	process(CLK)
+	begin
+		if (rising_edge(CLK)) then
+			-- handle input/output routing
+			if (TEST_EN = '1' or counter /=0) then
+				if (counter =  (2**OUTPUT_SIZE)-1) then
+					TEST_RUNNING<='0';
+					TEST_DONE<='1';
+					counter<=0;
+					INPUT_OUT <= INPUT_IN;
+					s_OUTPUT_OUT <= OUTPUT_IN;
+					
+					VECTOR_OUT <= comp_out;
+					if (comp_out = VECTOR) then
+						TEST_FAIL<='0';
+					else
+						TEST_FAIL<='1';
+					end if;
+				else
+					TEST_FAIL<='0';
+					TEST_DONE<='0';
+					TEST_RUNNING<='1';
+					INPUT_OUT <= PRBS;
+					Comp_in <= OUTPUT_IN;
+					s_OUTPUT_OUT <= conv_std_logic_vector(0,OUTPUT_SIZE+1);
+					counter <= counter + 1;
+				end if;
+			else
+				counter <= 0;
+				INPUT_OUT <= INPUT_IN;
+				s_OUTPUT_OUT <= OUTPUT_IN;
+			end if;
+			
+		end if;
+	end process;
+end architecture;
+	
---
+++ DISPLAY_DECODER_BIST.vhd
@@ -0,0 +1,41 @@
+LIBRARY IEEE;
+USE  IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_UNSIGNED.all;
+
+ENTITY DISPLAY_DECODER_BIST IS
+	PORT(			
+			value		: IN  integer range 0 to 9;
+			update	: IN std_logic;
+			display	: OUT std_logic_vector(6 downto 0);
+			TEST_EN	: IN	std_logic;
+			TEST_DONE: OUT std_logic;
+			TEST_FAIL: OUT std_logic;
+			VECTOR_OUT: OUT std_logic_vector(7 downto 0)
+		);
+END DISPLAY_DECODER_BIST;
+
+ARCHITECTURE behv OF DISPLAY_DECODER_BIST IS	
+		signal INPUT_OUT	: std_logic_vector (3 downto 0);
+		signal OUTPUT_IN	: std_logic_vector (6 downto 0);
+		signal TEST	: std_logic;
+
+BEGIN
+
+	BIST: entity WORK.BIST
+		generic map(3,6,7)
+		port map(conv_std_logic_vector(value,4),
+					INPUT_OUT,
+					OUTPUT_IN,
+					display,
+					"11101110",
+					update,
+					TEST_EN,
+					TEST_done,
+					TEST_FAIL,
+					VECTOR_OUT);
+		
+	DISP: entity WORK.DISPLAY_DECODER
+		port map (conv_integer(INPUT_OUT),update,OUTPUT_IN);
+
+END architecture; 
